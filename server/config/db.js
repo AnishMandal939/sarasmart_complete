a8ff7eddaf4cff103af31a1d53696879cdac1eae
AnishMandal939 <mandalktaanees12@gmail.com>
2022-12-23 15:23:04 +0545

    mongo cloud setup with env added

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 try {
-  mongoose.connect("mongodb://localhost:27017/Ecommerce", {
+  // mongoose.connect("mongodb://localhost:27017/Ecommerce", {
+  mongoose.connect("mongodb+srv://root:passwords@qti.eomuqq9.mongodb.net/?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
